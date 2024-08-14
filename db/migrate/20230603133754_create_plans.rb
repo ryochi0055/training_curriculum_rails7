2c9d1e0de21902a89a0d6d1521d494cc0f36dacb
ryochi0055 <aredoggu@gmail.com>
2024-08-14 19:42:01 +0900

    Issue4修正

--- db/migrate/20230603133754_create_plans.rb
+++ db/migrate/20230603133754_create_plans.rb
@@ -3,6 +3,7 @@ class CreatePlans < ActiveRecord::Migration[7.0]
     create_table :plans do |t|
       t.string :plan
       t.date :date
+      t.text :calendars
       t.timestamps
     end
   end
